6b9474df2f67707894d5454928a31d1263cb8251
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-02-20 16:56:48 -0800

    dang...stuck

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,9 @@
            https://api.github.com/users/<your name>
 */
 
+Axios.get("https://api.github.com/users.taylorpalmer");
+console.log(Axios);
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
